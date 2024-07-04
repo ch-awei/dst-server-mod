7d07ea9f6fb151ed9c73d127fd15b6ce67501f64
awei <122901542@qq.com>
2024-07-04 21:24:30 +0800

    fix bug

--- modules/deploy_anywhere.lua
+++ modules/deploy_anywhere.lua
@@ -3,7 +3,7 @@ GLOBAL.setmetatable(env, {__index = function(t, k) return GLOBAL.rawget(GLOBAL, 
 local cfg = GetModConfigData("deploy_anywhere") or false
 
 if table.contains({1, 3}, cfg) then
-  for _, v in pairs(AllRecipes) do
+  for k, v in pairs(AllRecipes) do
     if v.build_distance and v.build_distance > 0 then
       AllRecipes[k].build_distance = 0
     end
@@ -13,25 +13,22 @@ if table.contains({1, 3}, cfg) then
   end
 end
 
-if table.contains({2, 3}, cfg) then
-  local not_modes = {DEPLOYMODE.CUSTOM, DEPLOYMODE.WATER, DEPLOYMODE.TURF}
-  local not_tags = {'boatbuilder', 'fertilizer'}
-  AddComponentPostInit('deployable', function(self, inst)
-    if inst:HasOneOfTags(not_tags) or table.contains(not_modes, self.mode) then
-      return
-    end
-    -- self.mode = DEPLOYMODE.ANYWHERE
-    self.spacing = DEPLOYSPACING.NONE or 0
-  end)
-end
-
 if not cfg then return end
 
+local not_modes = {DEPLOYMODE.CUSTOM, DEPLOYMODE.WATER, DEPLOYMODE.TURF}
+local not_tags = {'boatbuilder', 'fertilizer'}
 AddPrefabPostInitAny(function(inst)
   if inst.deploy_smart_radius and inst.deploy_smart_radius > 0 then
     inst.deploy_smart_radius = 0
   end
   if inst.deploy_extra_spacing and inst.deploy_extra_spacing > 0 then
     inst.deploy_extra_spacing = 0
+  end
+  local d = inst.components.deployable
+  if table.contains({2, 3}, cfg) and d ~= nil then
+    if inst:HasOneOfTags(not_tags) or table.contains(not_modes, d.mode) then
+      return
+    end
+    d.spacing = DEPLOYSPACING.NONE or 0
   end
 end)
